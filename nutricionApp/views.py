41cea8bd0cb202594af6779749102086260e4efd
Daniel Martinez <dmartinez@datacrm.com>
2016-06-24 20:14:22 -0500

    base de datos base montada

--- nutricionApp/views.py
+++ nutricionApp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from django.template import loader, Context, RequestContext
+#from nutricionApp.models import Profile
+from django.http import HttpResponse
+
+import os
+
+
+# Create your views here.
+
+def home(request):
+    template = loader.get_template("home.html")
+    context = RequestContext(request)
+    return HttpResponse({template.render(context)})
