46daf421adac81187c7fef2eacba5024cc0cac33
Daniel Martinez <dmartinez@datacrm.com>
2016-06-29 14:31:47 -0500

    base de datos

--- nutricionApp/admin.py
+++ nutricionApp/admin.py
@@ -1,5 +1,27 @@
 from django.contrib import admin
-from .models import Usuario
+from .models import USUARIO
+from .models import USUARIO_ALIMENTO_PORCION
+from .models import GRAMOSPORPORCION
+from .models import ALIMENTO
+from .models import CANTIDAD_NUTRIENTE_ALIMENTO
+from .models import NUTRIENTE
+from .models import NUTRIENTE_ETIQUETA
+from .models import FUNCION_LINEAL
+from .models import ETIQUETA
+from .models import RECOMENDACIONES
+from .models import REGLA
+from .models import PRECEDENTE_CONSECUENTE
 
 # Register your models here.
-admin.site.register(Usuario)
+admin.site.register(USUARIO)
+admin.site.register(USUARIO_ALIMENTO_PORCION)
+admin.site.register(GRAMOSPORPORCION)
+admin.site.register(ALIMENTO)
+admin.site.register(CANTIDAD_NUTRIENTE_ALIMENTO)
+admin.site.register(NUTRIENTE)
+admin.site.register(NUTRIENTE_ETIQUETA)
+admin.site.register(FUNCION_LINEAL)
+admin.site.register(ETIQUETA)
+admin.site.register(RECOMENDACIONES)
+admin.site.register(REGLA)
+admin.site.register(PRECEDENTE_CONSECUENTE)
--- nutricionApp/models.py
+++ nutricionApp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class Usuario(models.Model):
+class USUARIO(models.Model):
     fecha_nacimiento = models.DateTimeField()
     nombres = models.CharField(max_length=254, null=False)
     correo = models.EmailField(null=False,max_length=254)
@@ -13,3 +13,86 @@ class Usuario(models.Model):
     
     def __unicode__(self):
         return unicode(self.correo)
+class ALIMENTO(models.Model):
+    nombre = models.CharField(max_length=50, null=False)
+    codigo_fao = models.CharField(max_length=50, null=False)
+    tags = models.CharField(max_length=80, null=False)
+    
+    def __unicode__(self):
+        return unicode(self.nombre)
+class GRAMOSPORPORCION(models.Model):
+    nombre_porcion = models.CharField(max_length=254, null=False)
+    valor_gramos = models.DecimalField(max_digits=5, decimal_places=2)
+    alimento = models.IntegerField(ALIMENTO)
+    
+    def __unicode__(self):
+        return unicode(self.nombre_porcion)
+        
+class USUARIO_ALIMENTO_PORCION(models.Model):
+    usuario = models.ForeignKey(USUARIO)
+    alimento_porcion = models.ForeignKey(GRAMOSPORPORCION)
+    tamano_porcion = models.DecimalField(max_digits=15, decimal_places=3)
+    fecha_consumo = models.DateTimeField()
+    
+    def __unicode__(self):
+        return unicode(self.tamano_porcion)
+
+class NUTRIENTE(models.Model):
+    nombre = models.CharField(max_length=50, null=False)
+    
+    def __unicode__(self):
+        return unicode(self.nombre)
+
+class CANTIDAD_NUTRIENTE_ALIMENTO(models.Model):
+    nutriente = models.ForeignKey(ALIMENTO)
+    alimento = models.ForeignKey(NUTRIENTE)
+    cantidad_gr = models.DecimalField(max_digits=15, decimal_places=3)
+    
+    def __unicode__(self):
+        return unicode(self.nutriente)
+        
+class ETIQUETA(models.Model):
+    nombre = models.CharField(max_length=50, null=False)
+    
+    def __unicode__(self):
+        return unicode(self.nombre)        
+
+class NUTRIENTE_ETIQUETA(models.Model):
+    nutriente = models.ForeignKey(NUTRIENTE)
+    etiqueta = models.ForeignKey(ETIQUETA)
+    
+    def __unicode__(self):
+        return unicode(self.nutriente)
+        
+class FUNCION_LINEAL(models.Model):
+    m = models.DecimalField(max_digits=8, decimal_places=4)
+    b = models.DecimalField(max_digits=8, decimal_places=4)
+    conjunto = models.ForeignKey(NUTRIENTE_ETIQUETA)
+    x1 = models.DecimalField(max_digits=8, decimal_places=4)
+    x2 = models.DecimalField(max_digits=8, decimal_places=4)
+    
+    def __unicode__(self):
+        return unicode(self.conjunto)
+
+        
+class RECOMENDACIONES(models.Model):
+    descripcion = models.CharField(max_length=256, null=False)
+    
+    def __unicode__(self):
+        return unicode(self.descripcion)
+        
+class REGLA(models.Model):
+    nombre = models.CharField(max_length=50, null=False)
+    
+    def __unicode__(self):
+        return unicode(self.nombre)
+        
+class PRECEDENTE_CONSECUENTE(models.Model):
+    regla_id = models.ForeignKey(REGLA)
+    nutriente_etiqueta = models.ForeignKey(NUTRIENTE_ETIQUETA)
+    recomendacion = models.ForeignKey(RECOMENDACIONES)
+    tipo = models.CharField(max_length=1, null=False)
+    
+    def __unicode__(self):
+        return unicode(self.regla_id)
+
